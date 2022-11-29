c8f726e34a3fa8b4c03ff0d6c1ec84e8c12d73bd
Caleb Economy <caleb.economy@mail.wlc.edu>
2022-11-29 13:25:49 -0600

    Created new monsters

--- content/actors/monsters.h
+++ content/actors/monsters.h
@@ -1,3 +1,35 @@
 #pragma once
 
-namespace Monsters {}
+#include <iostream>
+
+#include "action.h"
+#include "engine.h"
+#include "monstertype.h"
+#include "rest.h"
+#include "move.h"
+
+namespace Monsters {
+std::unique_ptr<Action> default_behavior(Engine& engine, Monster& me);
+MonsterType goblin();
+std::unique_ptr<Action> default_behavior(Engine& engine, Monster& me) {
+    return std::make_unique<Rest>();
+}
+constexpr int default_speed{8};
+MonsterType goblin() {
+    int health = 2;
+    return {"goblin", default_speed, health, std::make_shared<None>(),
+            default_behavior};
+}
+
+MonsterType orc_shaman() {
+    int health = 4;
+    return {"orc_shaman", default_speed, health, std::make_shared<None>(),
+            default_behavior};
+}
+
+MonsterType demon_big() {
+    int health = 5;
+    return {"demon_big", default_speed, health, std::make_shared<None>(),
+            default_behavior};
+}
+}  
