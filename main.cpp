6eccb66aa94c42ea5a874d19c71f19127c19a6be
Caleb Economy <caleb.economy@mail.wlc.edu>
2022-11-29 16:46:52 -0600

    Goblin works!

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 #include "engine.h"
 #include "heros.h"
+#include "monsters.h"
+#include "monstertype.h"
+#include "monster.h"
 
 int main() {
     std::string filename{"settings.txt"};
     Settings settings = read_settings(filename);
     Engine engine{settings};
 
-    engine.create_hero(Heros::wizard);
-    MonsterType new_monster_type = Monsters::goblin(); 
-    engine.create_monster(new_monster_type); 
-    MonsterType new_monster_type = Monsters::orc_shaman(); 
-    engine.create_monster(new_monster_type); 
-    MonsterType new_monster_type = Monsters::demon_big(); 
-    engine.create_monster(new_monster_type); 
+    engine.create_hero(Heros::wizard);  
+    MonsterType new_monster_type = goblin();  
+    engine.create_monster(new_monster_type);    
     engine.run();
 }
