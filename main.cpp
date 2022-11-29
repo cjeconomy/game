6f2af335a340903c20e54c23e462a70b096a2282
Caleb Economy <caleb.economy@mail.wlc.edu>
2022-11-29 13:25:17 -0600

    Added Monsters

--- main.cpp
+++ main.cpp
@@ -7,5 +7,11 @@ int main() {
     Engine engine{settings};
 
     engine.create_hero(Heros::wizard);
+    MonsterType new_monster_type = Monsters::goblin(); 
+    engine.create_monster(new_monster_type); 
+    MonsterType new_monster_type = Monsters::orc_shaman(); 
+    engine.create_monster(new_monster_type); 
+    MonsterType new_monster_type = Monsters::demon_big(); 
+    engine.create_monster(new_monster_type); 
     engine.run();
 }
