eb51643614dd41544d24754dcd988c45e502a685
Caleb Economy <caleb.economy@mail.wlc.edu>
2022-11-29 13:25:33 -0600

    Need to add constructor yet

--- content/actions/move.cpp
+++ content/actions/move.cpp
@@ -12,7 +12,7 @@
 
 Move::Move(Vec direction) : direction{direction} {}
 
-Result Move::perform(Engine& engine) {
+Result Move::perform(Engine & engine) {
     Vec position = actor->get_position();
     Vec new_position = position + direction;
     actor->change_direction(direction);
@@ -21,11 +21,10 @@ Result Move::perform(Engine& engine) {
     if (tile.is_wall() || tile.actor) {
         return failure();
     } else if (tile.is_door()) {
-        Door& door = engine.dungeon.doors.at(position);
-        if (!door_is_open()) {
-            return alternative(OpenDoor{position});
-        }  // Will be replaced with return
-           // alternative(OpenDoor{position});
+        Door& door = engine.dungeon.doors.at(new_position);
+        if (!door.is_open()) {
+            return alternative(OpenDoor{new_position});
+        }
     } else {
         actor->move_to(new_position);
         return success();
@@ -33,9 +32,3 @@ Result Move::perform(Engine& engine) {
         // alternative.)
 }
 
-/* if(tile.is_door()) {
-    Door&door = engine.dungeon.doors.at(position)
-    if (!door.is_open()) {
-        return alternative(OpenDoor(door))
-    };
-}*/
