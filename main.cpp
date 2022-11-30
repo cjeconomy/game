712587f7d6d8b28c29dfdfefa18d2ba6abce898f
Caleb Economy <caleb.economy@mail.wlc.edu>
2022-11-30 09:25:21 -0600

    3 Monsters in the Game

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include "engine.h"
 #include "heros.h"
+#include "monster.h"
 #include "monsters.h"
 #include "monstertype.h"
-#include "monster.h"
 
 int main() {
     std::string filename{"settings.txt"};
     Settings settings = read_settings(filename);
     Engine engine{settings};
 
-    engine.create_hero(Heros::wizard);  
-    MonsterType new_monster_type = goblin();  
-    engine.create_monster(new_monster_type);    
+    engine.create_hero(Heros::wizard);
+    MonsterType new_monster_type = goblin();
+    engine.create_monster(new_monster_type);
+    MonsterType shaman_orc = orc_shaman();
+    engine.create_monster(shaman_orc);
+    MonsterType big_demon = demon_big();
+    engine.create_monster(big_demon);
     engine.run();
 }
