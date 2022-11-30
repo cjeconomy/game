b5fae25ea0190bb1ec2d7d5cec5e444b7a2886a1
Caleb Economy <caleb.economy@mail.wlc.edu>
2022-11-30 09:25:35 -0600

    2 more monsters added

--- content/actors/monsters.cpp
+++ content/actors/monsters.cpp
@@ -1,12 +1,22 @@
 #include "monsters.h"
 
-
-std::unique_ptr<Action> default_behavior(Engine&, Monster&) { 
-    return std::make_unique<Rest>(); 
-} 
+std::unique_ptr<Action> default_behavior(Engine&, Monster&) {
+    return std::make_unique<Rest>();
+}
 int default_speed = 8;
-MonsterType goblin() { 
-    int health = 2; 
+MonsterType goblin() {
+    int health = 2;
     return {"goblin", default_speed, health, std::make_shared<None>(),
-        default_behavior}; 
-} 
+            default_behavior};
+}
+MonsterType orc_shaman() {
+    int health = 3;
+    return {"orc_shaman", default_speed, health, std::make_shared<None>(),
+            default_behavior};
+}
+
+MonsterType demon_big() {
+    int health = 5;
+    return {"demon_big", default_speed, health, std::make_shared<None>(),
+            default_behavior};
+}
