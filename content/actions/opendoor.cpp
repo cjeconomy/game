a6703d76ae824e6094262f541f544627439c38a5
Caleb Economy <caleb.economy@mail.wlc.edu>
2022-12-01 21:04:42 -0600

    Opendoor files complete

--- content/actions/opendoor.cpp
+++ content/actions/opendoor.cpp
@@ -3,5 +3,21 @@
 #include <iostream>
 
 #include "actor.h"
+#include "dungeon.h"
 #include "engine.h"
 #include "updatefov.h"
+
+OpenDoor::OpenDoor(Vec position) : position{position} {}
+
+Result OpenDoor::perform(Engine& engine) {
+    Tile& tile = engine.dungeon.tiles(position);
+    if (tile.is_door()) {
+        Door& door = engine.dungeon.doors.at(position);
+        door.open();
+        tile.walkable = true;
+        engine.events.add(UpdateFOV{});
+        return success();
+    } else {
+        return failure();
+    }
+}
