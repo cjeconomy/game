f12b5486f334075cfafee2ba79c57105480b5e4b
Caleb Economy <caleb.economy@mail.wlc.edu>
2022-12-01 21:08:55 -0600

    All monsters created

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "engine.h"
 #include "heros.h"
-#include "monster.h"
 #include "monsters.h"
 #include "monstertype.h"
+#include "monster.h"
 
 int main() {
     std::string filename{"settings.txt"};
