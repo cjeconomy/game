7e9c334add46fe261f189a711142fe3ef8a4bed2
Caleb Economy <caleb.economy@mail.wlc.edu>
2022-12-02 14:43:27 -0600

    Wander works

--- content/actions/wander.cpp
+++ content/actions/wander.cpp
@@ -1,20 +1,21 @@
 #include "wander.h"
 
 
-
+#include "randomness.h"
 
 #include "actor.h"
 #include "engine.h"
 #include "monster.h"
 #include "move.h"
 #include "tile.h"
+#include "rest.h"
 
 
-Wander::Wander(Vec position) : position{position} {}
+
 
 Result Wander::perform(Engine& engine) {
-    Vec pos = actor->get_position();
-    std::vector<Vec> neighbors = engine.dungeon.neighbors(pos);
+    Vec position = actor->get_position();
+    std::vector<Vec> neighbors = engine.dungeon.neighbors(position);
     // randomize directions
     shuffle(std::begin(neighbors), std::end(neighbors));
     for (const Vec& neighbor : neighbors) {
@@ -24,4 +25,5 @@ Result Wander::perform(Engine& engine) {
             return alternative(Move{direction});
         }
     }
+    return alternative(Rest());
 }
