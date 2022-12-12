a122d905cc5044ad29fedcbbe30c3ab42ae8da0e
Caleb Economy <caleb.economy@mail.wlc.edu>
2022-12-11 22:20:00 -0600

    Monsters given weapons

--- content/actors/monsters.cpp
+++ content/actors/monsters.cpp
@@ -1,42 +1,45 @@
 #include "monsters.h"
-#include "monster.h"
-#include "randomness.h"
-#include "monstertype.h"
 
-std::unique_ptr<Action> default_behavior(Engine& engine, Monster& me) { 
-    // pursue Hero if monster can see him (If Hero sees me, I see him) 
-    if (me.is_visible() && engine.hero) { 
-        Vec from = me.get_position(); 
-        Vec to = engine.hero->get_position(); 
-        std::vector<Vec> path = engine.dungeon.calculate_path(from, to); 
-        if (path.size() > 1) { 
-            Vec direction = path.at(1) - path.at(0); 
-            return std::make_unique<Move>(direction); 
-        } 
-    } 
-    // Monster doesn't see Hero 
-    if (probability(66)) { 
-        return std::make_unique<Wander>(); 
-    } 
-    else { 
-        return std::make_unique<Rest>(); 
-    } 
+#include "knife.h"
+#include "monster.h"
+#include "monstertype.h"
+#include "randomness.h"
+#include "sword.h"
+#include "sword_big.h"
+
+std::unique_ptr<Action> default_behavior(Engine& engine, Monster& me) {
+    // pursue Hero if monster can see him (If Hero sees me, I see him)
+    if (me.is_visible() && engine.hero) {
+        Vec from = me.get_position();
+        Vec to = engine.hero->get_position();
+        std::vector<Vec> path = engine.dungeon.calculate_path(from, to);
+        if (path.size() > 1) {
+            Vec direction = path.at(1) - path.at(0);
+            return std::make_unique<Move>(direction);
+        }
+    }
+    // Monster doesn't see Hero
+    if (probability(66)) {
+        return std::make_unique<Wander>();
+    } else {
+        return std::make_unique<Rest>();
+    }
 }
 
 constexpr int default_speed = 8;
 MonsterType goblin() {
     int health = 2;
-    return {"goblin", default_speed, health, std::make_shared<None>(),
+    return {"goblin", default_speed, health, std::make_shared<Knife>(1),
             default_behavior};
 }
 MonsterType orc_shaman() {
     int health = 3;
-    return {"orc_shaman", default_speed, health, std::make_shared<None>(),
+    return {"orc_shaman", default_speed, health, std::make_shared<Sword>(1),
             default_behavior};
 }
 
 MonsterType demon_big() {
     int health = 5;
-    return {"demon_big", default_speed, health, std::make_shared<None>(),
+    return {"demon_big", default_speed, health, std::make_shared<Sword_big>(3),
             default_behavior};
 }
