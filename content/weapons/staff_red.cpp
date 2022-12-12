58ac61a9b22dc8d843b00be04d2980f5a4920b14
Caleb Economy <caleb.economy@mail.wlc.edu>
2022-12-12 14:00:04 -0600

    fear added

--- content/weapons/staff_red.cpp
+++ content/weapons/staff_red.cpp
@@ -1,5 +1,6 @@
 #include "staff_red.h"
 
+#include "actor.h"
 #include "engine.h"
 #include "hit.h"
 #include "swing.h"
@@ -8,5 +9,6 @@ Staff_red::Staff_red(int damage) : Weapon{"staff_red", damage} {}
 
 void Staff_red::use(Engine& engine, Actor& attacker, Actor& defender) {
     Vec direction = defender.get_position() - attacker.get_position();
+    defender.set_fear(true);
     engine.events.add(Swing{sprite, direction, defender, damage});
 }
